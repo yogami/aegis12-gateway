bbbc137c676ab321318bcae14a340f6abdba22e8
Yami Gopal <yogami@berlinailabs.de>
2026-04-30 15:07:06 +0200

    fix: remove zk-artifacts from .dockerignore so docker build can copy them

--- nextjs-demo/.next/static/chunks/webpack.js
+++ nextjs-demo/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "54035a53200a083e"; }
+/******/ 		__webpack_require__.h = function() { return "9adcc02ac46a592a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
